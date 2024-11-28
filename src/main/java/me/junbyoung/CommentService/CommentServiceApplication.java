25b2790c1119448c0f5a0fcfb6135450aec07a8c
junbyoung96 <junbyoung96@gmail.com>
2024-11-28 20:43:02 +0900

    글로벌 예외처리기 구현

--- src/main/java/me/junbyoung/CommentService/CommentServiceApplication.java
+++ src/main/java/me/junbyoung/CommentService/CommentServiceApplication.java
@@ -2,6 +2,8 @@ package me.junbyoung.CommentService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
